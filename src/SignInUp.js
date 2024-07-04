e501027d10ebe2613310e3186f2de9b930d06654
PaarthChandna <111995482+PaarthChandna@users.noreply.github.com>
2024-07-04 14:37:36 +0530

    SignUp Frame

---
+++ src/SignInUp.js
@@ -0,0 +1,11 @@
+const SignInUp = () => {
+   return( <div className="absolute  w-full my-40">
+        <div className="bg-black opacity-70 w-4/12 flex justify-center mx-auto h-48">
+        <div className="">
+        <p className="flex justify-center text-white  font-bold text-2xl mt-6">Sign In</p>
+        </div>
+        
+        </div>
+    </div>)
+}
+export default SignInUp;
