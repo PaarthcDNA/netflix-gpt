f95920b45912dc040da718d66d29f04d7ac1c141
PaarthChandna <111995482+PaarthChandna@users.noreply.github.com>
2024-07-04 22:06:02 +0530

    vhnges

--- src/App.js
+++ src/App.js
@@ -6,16 +6,16 @@ import { getAnalytics } from "firebase/analytics";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDIIKEQXpd0hNupdh1PhNsHA6Ujv3f1Ftw",
-//   authDomain: "netlflix-gpt.firebaseapp.com",
-//   projectId: "netlflix-gpt",
-//   storageBucket: "netlflix-gpt.appspot.com",
-//   messagingSenderId: "878946852651",
-//   appId: "1:878946852651:web:f1b588623cae9c8f6d2f6e",
-//   measurementId: "G-R708JC2PKZ"
-// };
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDIIKEQXpd0hNupdh1PhNsHA6Ujv3f1Ftw",
+  authDomain: "netlflix-gpt.firebaseapp.com",
+  projectId: "netlflix-gpt",
+  storageBucket: "netlflix-gpt.appspot.com",
+  messagingSenderId: "878946852651",
+  appId: "1:878946852651:web:f1b588623cae9c8f6d2f6e",
+  measurementId: "G-R708JC2PKZ"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
